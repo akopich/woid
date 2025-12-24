887448677489881e219407d032342f078694349e
Valeriy Avanesov <acopich@gmail.com>
2025-12-24 22:11:07 +0100

    simplify unit tests.

--- test/woid_test.cpp
+++ test/woid_test.cpp
@@ -174,7 +174,10 @@ constexpr auto AllStorages = hana::concat(MoveOnlyStorageTypes, CopyStorageTypes
 template <typename Value>
 struct ValueCntNuller : testing::Test {
     ValueCntNuller() { Value::cnt = 0; }
-    ~ValueCntNuller() { Value::cnt = 0; }
+    ~ValueCntNuller() {
+        EXPECT_EQ(Value::cnt, 0);
+        Value::cnt = 0;
+    }
 };
 
 struct AlternativeAllocatorResetter {
@@ -236,175 +239,121 @@ TEST(AnyBuilder, canBuild) {
 TYPED_TEST(CopyTypesTestCase, canInstantiateFromRef) {
     using Storage = TypeParam::Storage;
     using Value = TypeParam::Value;
-    {
-        Storage storage(static_cast<const Value&>(kInt));
-        ASSERT_EQ(any_cast<Value&>(storage).i, kInt);
-    }
-    ASSERT_EQ(TypeParam::Value::cnt, 0);
+    Storage storage(static_cast<const Value&>(kInt));
+    ASSERT_EQ(any_cast<Value&>(storage).i, kInt);
 }
 
 TYPED_TEST(MoveTestCaseWithBigObject, canInstantiateFromPtr) {
     using Storage = TypeParam::Storage;
     using Value = TypeParam::Value;
     using Alloc = Storage::Alloc;
-    {
-        auto* ptr = Alloc::template make<Value>(kInt);
-        Storage storage(kTransferOwnership, ptr);
-        ASSERT_EQ(any_cast<Value&>(storage).i, kInt);
-    }
-    ASSERT_EQ(TypeParam::Value::cnt, 0);
+    auto* ptr = Alloc::template make<Value>(kInt);
+    Storage storage(kTransferOwnership, ptr);
+    ASSERT_EQ(any_cast<Value&>(storage).i, kInt);
 }
 
 TYPED_TEST(MoveTestCase, canInstantiateFromRefRef) {
     using Storage = TypeParam::Storage;
     using Value = TypeParam::Value;
-    {
-        Value v{kInt};
-        Storage storage(std::move(v));
-        ASSERT_EQ(any_cast<Value&>(storage).i, kInt);
-    }
-    ASSERT_EQ(TypeParam::Value::cnt, 0);
+    Value v{kInt};
+    Storage storage(std::move(v));
+    ASSERT_EQ(any_cast<Value&>(storage).i, kInt);
 }
 
 TYPED_TEST(MoveTestCase, canInstantiateInPlaceAndMove) {
     using Storage = TypeParam::Storage;
     using Value = TypeParam::Value;
-    {
-        Storage storage(std::in_place_type<Value>, kInt);
-        ASSERT_EQ(any_cast<Value&>(storage).i, kInt);
-        Storage otherStorage = std::move(storage);
-        ASSERT_EQ(any_cast<Value&>(otherStorage).i, kInt);
-    }
-    ASSERT_EQ(TypeParam::Value::cnt, 0);
+    Storage storage(std::in_place_type<Value>, kInt);
+    ASSERT_EQ(any_cast<Value&>(storage).i, kInt);
+    Storage otherStorage = std::move(storage);
+    ASSERT_EQ(any_cast<Value&>(otherStorage).i, kInt);
 }
 
 TYPED_TEST(MoveTestCase, canInstantiateAndMove) {
     using Storage = TypeParam::Storage;
     using Value = TypeParam::Value;
-    {
-        Storage storage(Value{kInt});
-        {
-            Storage otherStorage = std::move(storage);
-            ASSERT_EQ(any_cast<Value&>(otherStorage).i, kInt);
-        }
-    }
-
-    ASSERT_EQ(Value::cnt, 0);
+    Storage storage(Value{kInt});
+    Storage otherStorage = std::move(storage);
+    ASSERT_EQ(any_cast<Value&>(otherStorage).i, kInt);
 }
 
 TYPED_TEST(MoveTestCase, canSelfMoveAssign) {
     using Storage = TypeParam::Storage;
     using Value = TypeParam::Value;
-    {
-        Storage storage(Value{kInt});
-        {
-            SUPPRESS_SELF_MOVE_START
-            storage = std::move(storage);
-            SUPPRESS_SELF_MOVE_END
-            ASSERT_EQ(any_cast<Value&>(storage).i, kInt);
-        }
-    }
-    ASSERT_EQ(Value::cnt, 0);
+    Storage storage(Value{kInt});
+    SUPPRESS_SELF_MOVE_START
+    storage = std::move(storage);
+    SUPPRESS_SELF_MOVE_END
+    ASSERT_EQ(any_cast<Value&>(storage).i, kInt);
 }
 
 TYPED_TEST(MoveTestCase, canMoveAssign) {
     using Storage = TypeParam::Storage;
     using Value = TypeParam::Value;
-    {
-        Storage storage(Value{kInt});
-        {
-            Storage otherStorage(Value{42});
-            otherStorage = std::move(storage);
-            ASSERT_EQ(any_cast<Value&>(otherStorage).i, kInt);
-        }
-    }
-    ASSERT_EQ(Value::cnt, 0);
+    Storage storage(Value{kInt});
+    Storage otherStorage(Value{42});
+    otherStorage = std::move(storage);
+    ASSERT_EQ(any_cast<Value&>(otherStorage).i, kInt);
 }
 
 TYPED_TEST(MoveTestCase, canSwap) {
     using Storage = TypeParam::Storage;
     using Value = TypeParam::Value;
-    {
-        Storage storage(Value{kInt});
-        {
-            Storage otherStorage(Value{42});
-            std::swap(storage, otherStorage);
-            ASSERT_EQ(any_cast<Value&>(otherStorage).i, kInt);
-            ASSERT_EQ(any_cast<Value&>(storage).i, 42);
-        }
-    }
-    ASSERT_EQ(Value::cnt, 0);
+    Storage storage(Value{kInt});
+    Storage otherStorage(Value{42});
+    std::swap(storage, otherStorage);
+    ASSERT_EQ(any_cast<Value&>(otherStorage).i, kInt);
+    ASSERT_EQ(any_cast<Value&>(storage).i, 42);
 }
 
 TYPED_TEST(MoveTestCase, canSwapHeterogeneously) {
     using Storage = TypeParam::Storage;
     using Value = TypeParam::Value;
-    {
-        Storage storage(Value{kInt});
-        {
-            Storage otherStorage(int{42});
+    Storage storage(Value{kInt});
+    Storage otherStorage(int{42});
 
-            std::swap(storage, otherStorage);
-            ASSERT_EQ(any_cast<Value&>(otherStorage).i, kInt);
-            ASSERT_EQ(any_cast<int>(storage), 42);
+    std::swap(storage, otherStorage);
+    ASSERT_EQ(any_cast<Value&>(otherStorage).i, kInt);
+    ASSERT_EQ(any_cast<int>(storage), 42);
 
-            std::swap(storage, otherStorage);
-            ASSERT_EQ(any_cast<Value&>(storage).i, kInt);
-            ASSERT_EQ(any_cast<int>(otherStorage), 42);
-        }
-    }
-    ASSERT_EQ(Value::cnt, 0);
+    std::swap(storage, otherStorage);
+    ASSERT_EQ(any_cast<Value&>(storage).i, kInt);
+    ASSERT_EQ(any_cast<int>(otherStorage), 42);
 }
 
 TYPED_TEST(MoveTestCase, canGetByRef) {
     using Storage = TypeParam::Storage;
     using Value = TypeParam::Value;
-    {
-        Storage storage(Value{kInt});
-        auto& valueRef = any_cast<Value&>(storage);
-        ASSERT_EQ(valueRef.i, kInt);
-        valueRef.i++;
-        ASSERT_EQ(valueRef.i, kInt + 1);
-        ASSERT_EQ(any_cast<Value&>(storage).i, kInt + 1);
-    }
-    ASSERT_EQ(Value::cnt, 0);
+    Storage storage(Value{kInt});
+    auto& valueRef = any_cast<Value&>(storage);
+    ASSERT_EQ(valueRef.i, kInt);
+    valueRef.i++;
+    ASSERT_EQ(valueRef.i, kInt + 1);
+    ASSERT_EQ(any_cast<Value&>(storage).i, kInt + 1);
 }
 
 TYPED_TEST(MoveTestCase, canGetByRefRef) {
     using Storage = TypeParam::Storage;
     using Value = TypeParam::Value;
-    {
-        Storage storage(Value{kInt});
-        auto value = any_cast<Value&&>(std::move(storage));
-        ASSERT_EQ(value.i, kInt);
-    }
-
-    ASSERT_EQ(Value::cnt, 0);
+    Storage storage(Value{kInt});
+    auto value = any_cast<Value&&>(std::move(storage));
+    ASSERT_EQ(value.i, kInt);
 }
 
 TYPED_TEST(MoveTestCase, canMoveAssignToMovedFrom) {
     using Storage = TypeParam::Storage;
     using Value = TypeParam::Value;
-    {
-        Storage storage{Value{42}};
-        Storage unused{std::move(storage)};
-        storage = Storage{Value{kInt}};
-        auto value = any_cast<Value&&>(std::move(storage));
-        ASSERT_EQ(value.i, kInt);
-    }
-
-    ASSERT_EQ(Value::cnt, 0);
+    Storage storage{Value{42}};
+    Storage unused{std::move(storage)};
+    storage = Storage{Value{kInt}};
+    auto value = any_cast<Value&&>(std::move(storage));
+    ASSERT_EQ(value.i, kInt);
 }
 
 TYPED_TEST(MoveTestCase, canGetConstRef) {
     using Storage = TypeParam::Storage;
-    using Value = TypeParam::Value;
-    {
-        const Storage s{kInt};
-        ASSERT_EQ(any_cast<const int&>(s), kInt);
-    }
-
-    ASSERT_EQ(Value::cnt, 0);
+    const Storage s{kInt};
+    ASSERT_EQ(any_cast<const int&>(s), kInt);
 }
 
 TYPED_TEST(StorageType, canGetByValue) {
@@ -421,60 +370,44 @@ TYPED_TEST(StorageType, canGetByValue) {
 TYPED_TEST(CopyTypesCopyStorageTestCase, canCopy) {
     using Storage = TypeParam::Storage;
     using Value = TypeParam::Value;
-    {
-        Storage storage(Value{kInt});
-        Storage other = storage;
-        ASSERT_EQ(any_cast<Value>(storage).i, kInt);
-        ASSERT_EQ(any_cast<Value>(other).i, kInt);
-        ASSERT_NE(&any_cast<Value&>(storage), &any_cast<Value&>(other));
-    }
-
-    ASSERT_EQ(Value::cnt, 0);
+    Storage storage(Value{kInt});
+    Storage other = storage;
+    ASSERT_EQ(any_cast<Value>(storage).i, kInt);
+    ASSERT_EQ(any_cast<Value>(other).i, kInt);
+    ASSERT_NE(&any_cast<Value&>(storage), &any_cast<Value&>(other));
 }
 
 TYPED_TEST(CopyTypesCopyStorageTestCase, canCopyAssign) {
     using Storage = TypeParam::Storage;
     using Value = TypeParam::Value;
-    {
-        Storage storage(Value{kInt});
-        Storage other(Value{42});
-        ASSERT_EQ(any_cast<Value>(other).i, 42);
-        other = storage;
-        ASSERT_EQ(any_cast<Value>(storage).i, kInt);
-        ASSERT_EQ(any_cast<Value>(other).i, kInt);
-        ASSERT_NE(&any_cast<Value&>(storage), &any_cast<Value&>(other));
-    }
-
-    ASSERT_EQ(Value::cnt, 0);
+    Storage storage(Value{kInt});
+    Storage other(Value{42});
+    ASSERT_EQ(any_cast<Value>(other).i, 42);
+    other = storage;
+    ASSERT_EQ(any_cast<Value>(storage).i, kInt);
+    ASSERT_EQ(any_cast<Value>(other).i, kInt);
+    ASSERT_NE(&any_cast<Value&>(storage), &any_cast<Value&>(other));
 }
 
 TYPED_TEST(CopyTypesCopyStorageTestCase, canSelfCopyAssign) {
     using Storage = TypeParam::Storage;
     using Value = TypeParam::Value;
-    {
-        Storage storage(Value{kInt});
-        SUPPRESS_SELF_COPY_START
-        storage = storage;
-        SUPPRESS_SELF_COPY_END
-        ASSERT_EQ(any_cast<Value>(storage).i, kInt);
-    }
-
-    ASSERT_EQ(Value::cnt, 0);
+    Storage storage(Value{kInt});
+    SUPPRESS_SELF_COPY_START
+    storage = storage;
+    SUPPRESS_SELF_COPY_END
+    ASSERT_EQ(any_cast<Value>(storage).i, kInt);
 }
 
 TYPED_TEST(CopyTypesCopyStorageTestCase, canCopyAssignToMovedFrom) {
     using Storage = TypeParam::Storage;
     using Value = TypeParam::Value;
-    {
-        Storage storage{Value{42}};
-        Storage unused{std::move(storage)};
-        Storage other{Value{kInt}};
-        storage = other;
-        auto value = any_cast<Value&&>(std::move(storage));
-        ASSERT_EQ(value.i, kInt);
-    }
-
-    ASSERT_EQ(Value::cnt, 0);
+    Storage storage{Value{42}};
+    Storage unused{std::move(storage)};
+    Storage other{Value{kInt}};
+    storage = other;
+    auto value = any_cast<Value&&>(std::move(storage));
+    ASSERT_EQ(value.i, kInt);
 }
 
 template <template <typename> typename TraitV, typename X, typename Y>
